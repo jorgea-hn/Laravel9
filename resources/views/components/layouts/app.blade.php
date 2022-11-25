958792cc68e0d7a2e7e1be947770f3e7b985124f
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2022-11-25 16:04:29 -0500

    Componentes de blade

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aprendible - @yield('title')</title>
-    <meta name="description" content="@yield('meta-description')">
+    <title>Aprendible - {{$title ?? ''}}</title>
+    <meta name="description" content="{{$metaDescription ?? 'Default meta description'}}">
 </head>
 <body>
-    @include('partial.nav')
+    <x-layouts.nav/>
+
     
-    @yield('content')
+    {{-- @yield('content') --}}
+    {{ $slot }}
 
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,12 @@
-@extends("layouts.app")
+{{-- @component('components.layout')
+    <h1>Inicio</h1>
+@endcomponent --}}
 
-@section('title', 'Home')
-@section('meta-description', 'Home meta-description')
-
-@section('content')
-    <h1>Inicio</h1> 
-@endsection
-
-
+{{-- la x apunta a la carpeta components  --}}
+<x-layouts.app title="Home" meta-description="Home meta description">
+    {{-- <x-slot name="title">
+        Home 
+    </x-slot> --}}
+    
+    <h1>Home</h1>
+</x-layouts.app>
