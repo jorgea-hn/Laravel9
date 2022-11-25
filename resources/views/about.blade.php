e6c93828cc126b08533f107236c982b2461c4cae
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2022-11-25 15:25:49 -0500

    Plantillas de blade - herencia

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
-</head>
-<body>
-@include('partial.nav')
-    About
-</body>
-</html>
+@extends("layouts.app")
+
+@section('title', 'About')
+@section('meta-description', 'About meta-description')
+
+@section('content')
+    <h1>About</h1> 
+@endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
-</head>
-<body>
-@include('partial.nav')
-    Contact
-</body>
-</html>
+@extends("layouts.app")
+
+@section('title', 'Contact')
+@section('meta-description', 'Contact meta-description')
+
+@section('content')
+    <h1>Contact</h1> 
+@endsection
